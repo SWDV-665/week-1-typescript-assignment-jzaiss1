b26156caa3782bbb117f4ec7e5d78e7c4c36d662
jzaiss1 <jzaiss1@live.maryville.edu>
2020-03-14 20:19:51 -0500

    finished

---
+++ grocery.js
@@ -0,0 +1,30 @@
+// John Zaiss (GitHub id jzaiss1)
+// SWDV 665: ADV TOPICS: SOFTWARE DVLPMNT 1W 20/SP2
+// Week 1 | TypeScript Assignment
+var Groceries = /** @class */ (function () {
+    function Groceries(itemName, quantity, price) {
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.price = price;
+        this.fullItemName = itemName;
+    }
+    return Groceries;
+}());
+function groceryItem(item) {
+    return "<td>" + item.itemName + "</td><td>" + item.quantity + "</td><td> $" + item.price + "</td>";
+}
+var grocertList = [
+    groceryItem(new Groceries("Milk", 4, 3.15)),
+    groceryItem(new Groceries("Bread", 2, 3.75)),
+    groceryItem(new Groceries("Apples", 8, 0.75)),
+    groceryItem(new Groceries("Eggs", 3, 1.75)),
+    groceryItem(new Groceries("Cereal", 3, 4.15)),
+    groceryItem(new Groceries("Juice", 1, 3.75))
+];
+var listTable = "<table>";
+for (var _i = 0, grocertList_1 = grocertList; _i < grocertList_1.length; _i++) {
+    var i = grocertList_1[_i];
+    listTable += "<tr>" + i + "</tr>";
+}
+listTable += "</table>";
+document.body.innerHTML = listTable;
---
+++ grocery.ts
@@ -0,0 +1,42 @@
+// John Zaiss (GitHub id jzaiss1)
+// SWDV 665: ADV TOPICS: SOFTWARE DVLPMNT 1W 20/SP2
+// Week 1 | TypeScript Assignment
+
+class Groceries {
+    fullItemName: string;
+    constructor(
+      public itemName: string,
+      public quantity: number,
+      public price: number
+    ) {
+      this.fullItemName = itemName;
+    }
+  }
+  
+  interface Item {
+    itemName: string;
+    quantity: number;
+    price: number;
+  }
+  
+  function groceryItem(item: Item) {
+    return "<td>" + item.itemName + "</td><td>" + item.quantity + "</td><td> $" + item.price + "</td>"
+  }
+  
+  let grocertList = [
+      groceryItem(new Groceries("Milk", 4, 3.15)), 
+      groceryItem(new Groceries("Bread", 2, 3.75)),
+      groceryItem(new Groceries("Apples", 8, 0.75)), 
+      groceryItem(new Groceries("Eggs", 3, 1.75)),
+      groceryItem(new Groceries("Cereal", 3, 4.15)), 
+      groceryItem(new Groceries("Juice", 1, 3.75))
+  ]
+
+  let listTable = "<table>";
+
+  for (let i of grocertList) {
+        listTable += "<tr>" + i + "</tr>";
+  }
+
+  listTable += "</table>"
+  document.body.innerHTML = listTable;
